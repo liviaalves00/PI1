fce18b46d478c5de47bc9dac0b82e92bfc64c778
liviaalves00 <liviataina.ltab@gmail.com>
2024-07-15 15:06:01 -0300

    ryan ze ruela

--- 09-07-24/script.js
+++ 09-07-24/script.js
@@ -1,32 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var p1 = document.getElementById('texto')
-    p1.innerHTML = 'Hello World!'
+document.addEventListener("DOMContentLoaded", function () {
+  var p1 = document.getElementById("texto");
+  p1.innerHTML = "Hello World!";
 
-    // this.documentElement('btnHello').addEventListener('click', helloWorld);
-    document.getElementById('btnCopiar').addEventListener('click', copiar);
-    copy()
+  document.getElementById("btnCopiar").addEventListener("click", copiar);
+  copy();
 });
 
-function helloWorld(){
-    alert("Hello World")
+document.addEventListener("DOMContentLoaded", function () {
+  var paragrafos = document.getElementsByTagName("p");
+  Array.from(paragrafos).forEach((p) => {
+    if (p.id !== "texto") p.innerHTML = "World Hello!!!";
+  });
+});
 
+function helloWorld() {
+  alert("Hello World");
 }
 
 function copiar() {
-    let texto = document.getElementById('caixaTexto').value;
-    document.getElementById('resultado2').innerHTML += texto + '<br>';
-
+  let texto = document.getElementById("caixaTexto").value;
+  document.getElementById("resultado2").innerHTML += texto + "<br>";
 }
 
-function copy(){
-    var btn = document.getElementById("btn")
-    var src = document.getElementById("src")
-    var out = document.getElementById("out")
-    btn.addEventListener("click",()=>{
-        out.innerHTML += src.value.toUpperCase() + '<br>';
-    })
+function copy() {
+  var btn = document.getElementById("btn");
+  var src = document.getElementById("src");
+  var out = document.getElementById("out");
+  btn.addEventListener("click", () => {
+    out.innerHTML += src.value.toUpperCase() + "<br>";
+  });
 }
 
-function calculator(){
-
-}
+function calculator() {}
